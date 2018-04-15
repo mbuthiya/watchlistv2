4440fdf838c0f4d0da18a90bb97c29abce2961e5
James Muriuki <jamesmuriukimbuthia@gmail.com>
2018-04-15 11:22:41 +0300

    Update test for the movie class

--- app/movie_test.py
+++ app/movie_test.py
@@ -12,14 +12,14 @@ class MovieTest(unittest.TestCase):
         Setup method before every test
         '''
 
-        self.new_movie=Movie(12,"Python Must Be Crazy",'A crazy Python series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,123)
+        self.new_movie=Movie(12,"Python Must Be Crazy",'A crazy Python series','khsjha27hbs',8.5,123)
 
 
     def test_instance(self):
         '''
         Testing the Instance of the variable created
         '''
-    
+
         self.assertTrue(isinstance(self.new_movie,Movie))
 
 if __name__ =="__main__":
