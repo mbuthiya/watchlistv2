14ac7a366e01e17f9a829e5bbb0b9b07301863e9
James Muriuki <jamesmuriukimbuthia@gmail.com>
2018-04-15 08:51:18 +0300

    Add app configuration
    Imported development configuration from the config file and added it to our
    app instance config dictionary

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+
 # Init app
 app = Flask(__name__)
+
+# setting up configuration
+from .config import DevelopmentConfig
+
+app.config.from_object(DevelopmentConfig)
 
 #import files
 from app import views
