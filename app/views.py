9af7527ebe9704b8540d3c1c1f5b3c252364ba3d
James Muriuki <jamesmuriukimbuthia@gmail.com>
2018-04-15 08:13:20 +0300

    Alter view module with view function
    Added the index view function to load the index page

---
+++ app/views.py
@@ -0,0 +1,15 @@
+# import render template function
+from flask import render_template
+
+# Import app instance
+from app import app
+
+# Generate view functions
+
+@app.route('/')
+def index():
+    '''
+    Index view function loaded to display homepage information
+    '''
+
+    return render_template('index.html')
