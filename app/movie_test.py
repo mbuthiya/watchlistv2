dfc7e61167fe306f3c37d57f18745506dbfea3c3
James Muriuki <jamesmuriukimbuthia@gmail.com>
2018-04-15 10:35:43 +0300

    Create instance test case

--- app/movie_test.py
+++ app/movie_test.py
@@ -6,4 +6,21 @@ class MovieTest(unittest.TestCase):
     '''
     Test class test the behaviour of our Model
     '''
-    pass
+
+    def setUp(self):
+        '''
+        Setup method before every test
+        '''
+
+        self.new_movie=Movie(12,"Python Must Be Crazy",'A crazy Python series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,123)
+
+
+    def test_instance(self):
+        '''
+        Testing the Instance of the variable created
+        '''
+    
+        self.assertTrue(isinstance(self.new_movie,Movie))
+
+if __name__ =="__main__":
+    unittest.main()
