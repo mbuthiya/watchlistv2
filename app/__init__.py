3b51b84e3dcc5387097aa9208c024052ebc8bbcf
James Muriuki <jamesmuriukimbuthia@gmail.com>
2018-04-15 09:34:16 +0300

    Add instnce config
    added the instance path to the app instnce and added the config.py
    to the app.config dictionary

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 # Init app
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config=True)
 
 # setting up configuration
 from .config import DevelopmentConfig
 
 app.config.from_object(DevelopmentConfig)
+app.config.from_pyfile("config.py")
+
 
 #import files
 from app import views
