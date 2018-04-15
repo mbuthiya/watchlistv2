25ca3789f6184fbea2df5e6fe0565aa36484b994
James Muriuki <jamesmuriukimbuthia@gmail.com>
2018-04-15 11:18:12 +0300

    Add process results function
    Added a process results function inside our request.py file to process
    the movies ditionary list into a list of movie objects

--- app/request.py
+++ app/request.py
@@ -35,3 +35,43 @@ def get_movies(category):
         final_movie_list = []
 
     return final_movie_list
+
+
+
+def process_movie_results(unedited_movie_list):
+    '''
+    Takes in a dictionary of movies and loops through them creating instances of the Movie class with the properities needed.
+
+    Args:
+        unedited_movie_list a list of movie dictionaries
+    '''
+
+    processed_movie_list =[]
+
+    for movie_dict in unedited_movie_list:
+        if approved_movie(movie_dict):
+            id = movie_dict.get("id")
+            title=movie_dict.get("original_title")
+            overview=movie_dict.get("overview")
+            image =movie_dict.get("poster_path")
+            vote_average =movie_dict.get("vote_average")
+            vote_count =movie_dict.get("vote_count")
+
+            # Generate movie object
+            movie_dict_object = Movie(id,title,overview,image,vote_average,vote_count)
+
+    return processed_movie_list
+
+
+def approved_movie(movie_dict):
+    '''
+    Checks to see if all the properities are available to create a new movie object
+
+    Args:
+        movie_dict : A movie dictionary
+    '''
+
+    if movie_dict["id"] and movie_dict["original_title"] and movie_dict["overview"] and movie_dict["poster_path"] and movie_dict["vote_average"] and movie_dict["vote_count"]:
+        return True
+    else:
+        return False
